d14b5fc6b5a38e0afb9689e3856cff35ffdf76cd
David <daviddenoia72@gmail.com>
2025-02-05 12:27:10 +0000

    Agregar archivos de proyecto.

---
+++ TFTService/NumToCatService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TFTService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface NumToCat
+    {
+
+        [OperationContract]
+        PartesNumeros GetNumber(string value);
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class PartesNumeros
+    {
+        string parteEntera = "";
+        string parteDecimal = "";
+
+        [DataMember]
+        public string ParteEntera
+        {
+            get { return parteEntera; }
+            set { parteEntera = value; }
+        }
+
+        [DataMember]
+        public string ParteDecimal
+        {
+            get { return parteDecimal; }
+            set { parteDecimal = value; }
+        }
+    }
+}
---
+++ TFTService/NumToCatService.svc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TFTService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class TFTServicio : NumToCat
+    {
+        public PartesNumeros GetNumber(string value)
+        {
+
+            bool tieneComa=false;
+
+            if (String.IsNullOrEmpty(value))
+            {
+                return new PartesNumeros { ParteEntera= "", ParteDecimal=""};
+            }
+
+            for(int i=0; i<value.Length; i++)
+            {
+                char caracter = value[i];
+
+                if(!char.IsDigit(caracter) && caracter != ',')
+                {
+                    return new PartesNumeros { ParteEntera = "", ParteDecimal = "" };
+
+                }
+
+                if(caracter == ',')
+                {
+                    tieneComa = true;
+                }
+
+            }
+
+            if (tieneComa)
+            {
+                string[] partes = value.Split(',');
+
+                if (partes.Length != 2) {
+
+                    return new PartesNumeros { ParteEntera = "", ParteDecimal = "" };
+                }
+
+                string parteEntera = partes[0];
+                string parteDecimal = partes[1];
+
+                return new PartesNumeros { ParteEntera = parteEntera, ParteDecimal = parteDecimal };
+
+            }
+
+            return new PartesNumeros { ParteEntera = value, ParteDecimal=""};
+        }
+
+       
+
+    }
+}

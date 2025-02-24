9708327a9088183c761947f000b57c52d9777a8c
David <daviddenoia72@gmail.com>
2025-02-24 16:02:04 +0000

    Añadido web form con un poco de back ya esta conectado el web form con el servicio wcf

---
+++ TFTWebApplication/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TFTWebApplication._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1>Conversor de Números</h1>
+
+    <div class="form-group">
+        <label for="txtNumero">Introduce un número:</label>
+        <asp:TextBox ID="txtNumero" CssClass="form-control" runat="server"></asp:TextBox>
+    </div>
+
+    <div class="form-group">
+        <asp:Button ID="Button1" runat="server" Height="28px" OnClick="Traducir_Click" Text="Traducir" Width="118px" />
+    </div>
+
+    <br />
+
+    <asp:Label ID="lblResultado" runat="server" ForeColor="Blue" CssClass="lead"></asp:Label>
+
+    <asp:Repeater ID="rptResultados" runat="server">
+        <ItemTemplate>
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title"><strong>Tipo:</strong> <%# Eval("Tipo") %></h5>
+                    <p class="card-text"><strong>Respuesta:</strong> <%# string.Join(", ", (string[])Eval("Respuestas")) %></p>
+                </div>
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
+
+</asp:Content>
---
+++ TFTWebApplication/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TFTWebApplication.ServiceReference1;
+namespace TFTWebApplication
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                rptResultados.DataSource = null;
+                rptResultados.DataBind();
+            }
+
+        }
+
+        protected void Traducir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string numero = txtNumero.Text;
+                string lenguaje = "ca-CA";
+
+                if (string.IsNullOrEmpty(numero))
+                {
+                    lblResultado.Text = "Introducir numero valido";
+                    return;
+                }
+
+                using (var cliente = new NumToCatClient())
+                {
+                    List<Conversion> resultados = cliente.MainTraducir(numero, lenguaje).ToList();
+                    if (resultados != null && resultados.Count > 0)
+                    {
+                        rptResultados.DataSource = resultados;
+                        rptResultados.DataBind();
+                        lblResultado.Text = "Conversion hecha";
+
+                    }
+                    else
+                    {
+                        lblResultado.Text = "No se encontraron conversiones";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResultado.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
